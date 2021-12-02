8458ea45339720f062f8a81f5ab553906986f889
Aditya Pro <85778064+adityapro11@users.noreply.github.com>
2021-12-02 11:48:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function fname(){
+    document.getElementById('tname').innerHTML = 'Aditya'
+}
+
+function fage(){
+    document.getElementById('tage').innerHTML = '10years'
+}
+
+function fclass(){
+    document.getElementById('tclass').innerHTML = '6th'
+}
+
+function fschool (){
+    document.getElementById('tschool').innerHTML = 'CCWS'
+}
+
+function aboutme(){
+    document.getElementById('paboutme').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eum debitis, molestiae repellendus error rem minima itaque a recusandae, maxime quam cupiditate animi iste quae, est necessitatibus amet voluptas! Laudantium Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti dolorem velit pariatur ipsam? Deserunt, aspernatur? Praesentium animi dolores nam sint aspernatur. Repellendus harum animi dolore alias libero, dicta facilis illum.'
+}
